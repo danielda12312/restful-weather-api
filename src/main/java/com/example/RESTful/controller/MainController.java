8b15b7e87298bab02f46f435d0de5ec611450a6d
danielda12312 <danielda12312@gmail.com>
2025-11-24 17:33:28 +0200

    First Controller prints Greetings as plain text on localhost:8080

--- src/main/java/com/example/RESTful/controller/MainController.java
+++ src/main/java/com/example/RESTful/controller/MainController.java
@@ -1,4 +1,13 @@
 package com.example.RESTful.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+    @GetMapping("/")
+    private String index()
+    {
+        return "Greetings!";
+    }
 }
